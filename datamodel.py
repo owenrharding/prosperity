317b33a302f47d2ad43b7048452d9cc564ed7ded
owenrharding <owenrharding@gmail.com>
2024-03-28 20:00:42 +1000

    Reverted files to default

--- datamodel.py
+++ datamodel.py
@@ -1,15 +1,83 @@
-"""
-TradingState class: Holds all information that algorithm needs to make decisions about which orders to send.
+import json
+from typing import Dict, List
+from json import JSONEncoder
+import jsonpickle
 
-own_trades: Dictionary of Trades the algorithm has done since the last TradingState came in.
-market_trades: Dictionary of Trades the other market participants have done since the last TradingState came in.
-position: Dictionary of long or short position that we hold in every tradable product.
-order_depths: Dictionary of all the buy and sell orders per product that other market participants have sent and the algorithm is able to trade with. 
-"""
 Time = int
 Symbol = str
 Product = str
 Position = int
+UserId = str
+ObservationValue = int
+
+
+class Listing:
+
+    def __init__(self, symbol: Symbol, product: Product, denomination: Product):
+        self.symbol = symbol
+        self.product = product
+        self.denomination = denomination
+        
+                 
+class ConversionObservation:
+
+    def __init__(self, bidPrice: float, askPrice: float, transportFees: float, exportTariff: float, importTariff: float, sunlight: float, humidity: float):
+        self.bidPrice = bidPrice
+        self.askPrice = askPrice
+        self.transportFees = transportFees
+        self.exportTariff = exportTariff
+        self.importTariff = importTariff
+        self.sunlight = sunlight
+        self.humidity = humidity
+        
+
+class Observation:
+
+    def __init__(self, plainValueObservations: Dict[Product, ObservationValue], conversionObservations: Dict[Product, ConversionObservation]) -> None:
+        self.plainValueObservations = plainValueObservations
+        self.conversionObservations = conversionObservations
+        
+    def __str__(self) -> str:
+        return "(plainValueObservations: " + jsonpickle.encode(self.plainValueObservations) + ", conversionObservations: " + jsonpickle.encode(self.conversionObservations) + ")"
+     
+
+class Order:
+
+    def __init__(self, symbol: Symbol, price: int, quantity: int) -> None:
+        self.symbol = symbol
+        self.price = price
+        self.quantity = quantity
+
+    def __str__(self) -> str:
+        return "(" + self.symbol + ", " + str(self.price) + ", " + str(self.quantity) + ")"
+
+    def __repr__(self) -> str:
+        return "(" + self.symbol + ", " + str(self.price) + ", " + str(self.quantity) + ")"
+    
+
+class OrderDepth:
+
+    def __init__(self):
+        self.buy_orders: Dict[int, int] = {}
+        self.sell_orders: Dict[int, int] = {}
+
+
+class Trade:
+
+    def __init__(self, symbol: Symbol, price: int, quantity: int, buyer: UserId=None, seller: UserId=None, timestamp: int=0) -> None:
+        self.symbol = symbol
+        self.price: int = price
+        self.quantity: int = quantity
+        self.buyer = buyer
+        self.seller = seller
+        self.timestamp = timestamp
+
+    def __str__(self) -> str:
+        return "(" + self.symbol + ", " + self.buyer + " << " + self.seller + ", " + str(self.price) + ", " + str(self.quantity) + ", " + str(self.timestamp) + ")"
+
+    def __repr__(self) -> str:
+        return "(" + self.symbol + ", " + self.buyer + " << " + self.seller + ", " + str(self.price) + ", " + str(self.quantity) + ", " + str(self.timestamp) + ")"
+
 
 class TradingState(object):
 
@@ -34,83 +102,8 @@ class TradingState(object):
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True)
 
+    
+class ProsperityEncoder(JSONEncoder):
 
-"""
-Trade class: A transaction of a product.
-
-symbol: Product that the trade corresponds to.
-price: The price that product was exchanged.
-quantity: The quantity that was exchanged.
-buyer: The identity of the buyer in the transaction.
-seller: The identity of the seller in the transaction.
-"""
-Symbol = str
-UserId = str
-
-class Trade:
-
-    def __init__(self, symbol: Symbol, price: int, quantity: int, buyer: UserId = None, seller: UserId = None, timestamp: int = 0) -> None:
-        self.symbol = symbol
-        self.price: int = price
-        self.quantity: int = quantity
-        self.buyer = buyer
-        self.seller = seller
-        self.timestamp = timestamp
-
-    def __str__(self) -> str:
-        return "(" + self.symbol + ", " + self.buyer + " << " + self.seller + ", " + str(self.price) + ", " + str(self.quantity) + ", " + str(self.timestamp) + ")"
-
-    def __repr__(self) -> str:
-        return "(" + self.symbol + ", " + self.buyer + " << " + self.seller + ", " + str(self.price) + ", " + str(self.quantity) + ", " + str(self.timestamp) + ")" + self.symbol + ", " + self.buyer + " << " + self.seller + ", " + str(self.price) + ", " + str(self.quantity) + ")"
-
-
-"""
-OrderDepth class: Collection of all outstanding buy and sell orders for a particular product.
-
-In both of these dictionaries, the keys are the price and the values are the total volume on that price level.
-E.g. If buy_orders was {9: 5, 10: 4}, that means there is a total buy order quantity at price level 9, and a total buy order quantity of 4 at price level 10.
-"""
-class OrderDepth:
-
-    def __init__(self):
-        self.buy_orders: Dict[int, int] = {}
-        self.sell_orders: Dict[int, int] = {}
-
-
-"""
-Observation class: 
-
-In TradingState, two observation items are given. #Note: can only see one instance of observation in TradingState? #
-One is a simple product to value dictionary inside plainValueObservations.
-The other is a dictionary of complex ConversionObservation values for respective products. Used to place conversion requests from Trader class. Structure visible below.
-"""
-class ConversionObservation:
-
-    def __init__(self, bidPrice: float, askPrice: float, transportFees: float, exportTariff: float, importTariff: float, sunlight: float, humidity: float):
-        self.bidPrice = bidPrice
-        self.askPrice = askPrice
-        self.transportFees = transportFees
-        self.exportTariff = exportTariff
-        self.importTariff = importTariff
-        self.sunlight = sunlight
-        self.humidity = humidity
-
-
-"""
-Order: Proposed order for a product placed by the run() function in Trader class.
-
-I think the difference between an Order and a Trade is that an Order is proposed but may not be accepted, while a Trade is a confirmed Order?
-"""
-Symbol = str
-
-class Order:
-    def __init__(self, symbol: Symbol, price: int, quantity: int) -> None:
-        self.symbol = symbol
-        self.price = price
-        self.quantity = quantity
-
-    def __str__(self) -> str:
-        return "(" + self.symbol + ", " + str(self.price) + ", " + str(self.quantity) + ")"
-
-    def __repr__(self) -> str:
-        return "(" + self.symbol + ", " + str(self.price) + ", " + str(self.quantity) + ")"
+        def default(self, o):
+            return o.__dict__
--- intelliTraders.py
+++ intelliTraders.py
@@ -1,23 +1,18 @@
 from datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
-from math import *
 import string
 
-"""
-Trader class: coding logic contained in here.
-
-Single method run(), which is called every time a new TradingState is available.
-Should return a dictionary name result which contains all the orders the algorithm in run() decides to send.
-"""
 class Trader:
-
+    
     def run(self, state: TradingState):
-        # Result should be a dictionary of all the orders that the algorithm decides to spend
+        # Only method required. It takes all buy and sell orders for all symbols as an input, and outputs a list of orders to be sent
+        print("traderData: " + state.traderData)
+        print("Observations: " + str(state.observations))
         result = {}
         for product in state.order_depths:
             order_depth: OrderDepth = state.order_depths[product]
             orders: List[Order] = []
-            acceptable_price = 10  # Participant should calculate this value
+            acceptable_price = 10;  # Participant should calculate this value
             print("Acceptable price : " + str(acceptable_price))
             print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
     
@@ -34,45 +29,9 @@ class Trader:
                     orders.append(Order(product, best_bid, -best_bid_amount))
             
             result[product] = orders
-        return result
-
-    def calculate_z_score(product, priceListOfProduct ...):
-        # I'm thinking that, rather than calculating the mean of each product
-        # each time we get a new TradingState, we should calculate it once, store the mean of
-        # each product somewhere, and then update it when we get a new TradingState
-        # However, the following is how you calculate mean reversion, super simple:
-
-
-        # Get mean of product: mean = sumOfPrices/numberOfObservations
-        mean = 0
-        numberOfObservations = len(priceList)
-        sumOfPrices = 0
-
-        for price in priceList:
-            sumOfPrices += price
+    
+    
+        traderData = "SAMPLE" # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
         
-        mean = sumOfPrices/numberOfObservations
-
-        # Find current deviation from mean: deviation = price - mean
-        deviation = priceList[-1] - mean # I think this is the syntax for the lastmost index of a list? I forgot :/
-
-        # Find standard deviation: standardDeviation = squareRoot(sumOfSquaredDeviations/(Number of Observations - 1))
-        sumOfSquaredDeviations = 0
-        for price in priceList:
-            priceDevSquared = (price - mean) ** 2
-            sumOfSquaredDeviations += priceDevSquared
-        
-        meanOfSquaredDeviations = sumOfSquaredDeviations/numberOfObservations
-        standardDeviation = sqrt(meanOfSquaredDeviations) # Note: this is different to how I wrote it up there ^, the way I've done it here is how I found it on MathsIsFun. Not sure if it's right but it'll do for now
-
-        # Calculate Z-score: zScore = deviation/standardDeviation
-        zScore = deviation/standardDeviation
-
-        # Then, a Z-score above a certain threshold (commonly 1.5 or 2) may indicate the asset is overvalued, and
-        # below a certain threshold (commonly -1.5 or -2) may indicate the asset is undervalued.
-        
-
-        # From here, could use different strategies such as Moving Averages, Bollinger Bands, Relative Strength Index, Stochastic Oscillator, or Moving Average Convergence Divergence
-        # Mean reversion has limitations, which include the fact that it is less effective in strongly trending markets, where prices may not revert to the mean for extended periods.
-        # If there's a way to calculate this, we could do some logic on a ratio of how much we take mean_reversion into account, and how much we take a different strategy into account
-        pass
+        conversions = 1
+        return result, conversions, traderData

0078fc9c63de8e9846910165061c62b7dc2d0efd
owenrharding <owenrharding@gmail.com>
2024-03-28 21:33:30 +1000

    Guard clauses for position limits
    Tried adding some guard clauses on the position limits so the algo has a capped amount on how much it can buy or sell

--- trader.py
+++ trader.py
@@ -4,8 +4,11 @@ import string
 
 class Trader:
     
-    POSITION_LIMITS = {'AMETHYST': 20, 'STARFUIT': 20}
+    POSITION_LIMIT = {'AMETHYST': 20, 'STARFUIT': 20}
 
+    def determine_acceptable_price(state: TradingState):
+        pass
+    
     def run(self, state: TradingState):
         # Only method required. It takes all buy and sell orders for all symbols as an input, and outputs a list of orders to be sent
         print("traderData: " + state.traderData)
@@ -23,21 +26,37 @@ class Trader:
         for product in state.order_depths:
             order_depth: OrderDepth = state.order_depths[product]
             orders: List[Order] = []
-            acceptable_price = 10;  # Participant should calculate this value
+
+            acceptable_price = 5  # Participant should calculate this value
+
             print("Acceptable price : " + str(acceptable_price))
             print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
+
+            currentProductPosition = state.position[product]
+
+            ### BUY ### 
+            # If current position is less than the position limit, then it's okay to buy
+            if currentProductPosition < self.POSITION_LIMIT[product]:
+                # Buying the max buy quantity would place our position at the upper position limit
+                maxBuyQty = self.POSITION_LIMIT[product] - currentProductPosition
+
+                if len(order_depth.sell_orders) != 0:
+                    best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+                    if int(best_ask) < acceptable_price:
+                        print("BUY", str(-best_ask_amount) + "x", best_ask)
+                        orders.append(Order(product, best_ask, maxBuyQty))
     
-            if len(order_depth.sell_orders) != 0:
-                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-                if int(best_ask) < acceptable_price:
-                    print("BUY", str(-best_ask_amount) + "x", best_ask)
-                    orders.append(Order(product, best_ask, -best_ask_amount))
-    
-            if len(order_depth.buy_orders) != 0:
-                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                if int(best_bid) > acceptable_price:
-                    print("SELL", str(best_bid_amount) + "x", best_bid)
-                    orders.append(Order(product, best_bid, -best_bid_amount))
+            ### SELL ###
+            # If current position is greater than or equal to the position limit, then it's okay to sell
+            if currentProductPosition >= self.POSITION_LIMIT[product]:
+                # Selling the max sell quantity would place our position at the lower position limit
+                maxSellQty = currentProductPosition - self.POSITION_LIMIT[product]
+
+                if len(order_depth.buy_orders) != 0:
+                    best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+                    if int(best_bid) > acceptable_price:
+                        print("SELL", str(best_bid_amount) + "x", best_bid)
+                        orders.append(Order(product, best_bid, maxSellQty))
             
             result[product] = orders
     

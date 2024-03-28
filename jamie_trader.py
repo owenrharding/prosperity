8230599383de1f072f1d30bde75b2962b78b985e
Jamie0249 <165127010+Jamie0249@users.noreply.github.com>
2024-03-28 23:13:55 +1000

    Update jamie_trader.py
    i might havecancer

--- jamie_trader.py
+++ jamie_trader.py
@@ -1,33 +1,30 @@
 from datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
-import string
 
 class Trader:
 
     POSITION_LIMIT = {'AMETHYST': 20, 'STARFRUIT': 20}
     
     def run(self, state: TradingState):
-        # Only method required. It takes all buy and sell orders for all symbols as an input, and outputs a list of orders to be sent
-        print("traderData: " + state.traderData)
-        print("Observations: " + str(state.observations))
-        
         result = {'AMETHYST': [], 'STARFRUIT': []}
+        
         for product in state.order_depths:
-            acceptable_price = 5
             order_depth: OrderDepth = state.order_depths[product]
             orders: List[Order] = []
-            currentProductPosition = state.position.get(product, 0)
+            current_product_position = state.position.get(product, 0)
             
-            if currentProductPosition < self.POSITION_LIMIT[product]:
+            if current_product_position < self.POSITION_LIMIT[product]:
                 if len(order_depth.sell_orders) != 0:
                     best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+                    acceptable_price = 5  # Participant should calculate this value
                     if int(best_ask) < acceptable_price:
-                        print(f"BUY {product}: {str(-best_ask_amount)}x at {best_ask}")
-                        orders.append(Order(product, best_ask, -best_ask_amount))
+                        print(f"BUY {product}: {str(best_ask_amount)}x at {best_ask}")
+                        orders.append(Order(product, best_ask, best_ask_amount))
     
-            if currentProductPosition >= self.POSITION_LIMIT[product]:
+            if current_product_position >= self.POSITION_LIMIT[product]:
                 if len(order_depth.buy_orders) != 0:
                     best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+                    acceptable_price = 5  # Participant should calculate this value
                     if int(best_bid) > acceptable_price:
                         print(f"SELL {product}: {str(best_bid_amount)}x at {best_bid}")
                         orders.append(Order(product, best_bid, -best_bid_amount))
@@ -35,10 +32,9 @@ class Trader:
             result[product] = orders
     
         # Update trader data if needed
-        traderData = "SAMPLE"
+        trader_data = "SAMPLE"
         
         # Assume conversions for simplicity
         conversions = 1
-        timestamp = 0
-       
-        return result, conversions, traderData, timestamp
+        
+        return result, conversions, trader_data

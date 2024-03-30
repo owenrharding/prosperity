c96dd7aeb16839e4508e8a7850a5fa13e57809ed
mngv7 <165127020+mngv7@users.noreply.github.com>
2024-03-30 17:11:59 +1000

    Update zack_trader.py

--- zack_trader.py
+++ zack_trader.py
@@ -22,12 +22,14 @@ class Trader:
             if len(order_depth.sell_orders) != 0:
                 best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
                 if int(best_ask) < acceptable_price:
+                    print("Best ask: " + best_ask)
                     print("BUY", str(-best_ask_amount) + "x", best_ask)
                     orders.append(Order(product, best_ask, -best_ask_amount))
     
             if len(order_depth.buy_orders) != 0:
                 best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
                 if int(best_bid) > acceptable_price:
+                    print("Best Bid: " + best_bid)
                     print("SELL", str(best_bid_amount) + "x", best_bid)
                     orders.append(Order(product, best_bid, -best_bid_amount))
             

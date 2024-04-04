5942d4d42b77780c8d486a473b60a6ee3393118f
owenrharding <owenrharding@gmail.com>
2024-04-04 10:58:49 +0800

    Renamed new_trader to owen_linear_regression
    Takes the average of the last 10 or 20 GTVs and uses that as the acceptable price

--- owen_linear_regression.py
+++ owen_linear_regression.py
@@ -9,9 +9,13 @@ class Trader:
     # Define position limits for each product.
     POSITION_LIMIT = {'AMETHYSTS': 20, 'STARFRUIT': 20}
 
-    # Define historical data arrays. This will store previously calculated acceptable prices and the corresponding timestamps.
-    PRICE_HISTORY = {'AMETHYSTS': {}, 'STARFRUIT': {}}# Got this from stack overflow, not sure if it works.
+    # Define price history dictionaries for each product. These will store every historical acceptable price calculated by the algorithm.
+    # These are stored in dictionaries, where timestamp is the key, and the acceptable price is the value.
+    PRICE_HISTORY = {'AMETHYSTS': {}, 'STARFRUIT': {}}
 
+    """
+    Updates price history arrays stored in Trader class.
+    """
     def update_price_history(self, product: str, acceptable_price: float, state: TradingState):
         # Add acceptable price to corresponding product in PRICE_HISTORY dictionary
         if product in self.PRICE_HISTORY:
@@ -19,35 +23,37 @@ class Trader:
             product_dict[state.timestamp] = acceptable_price
     
     """
-    A modular function that returns the predicted value of a dependent variable at an increment of an independent
+    Returns the predicted value of a dependent variable at an increment of an independent
     variable based on simple linear regression.
     It's assumed that the dictionary of data given to this function has the keys as the independent variable and the values
     as the dependent variable.
     It's pretty much assumed that the indepdenent variable is the timestamp and the dependent variable is the price.
     """
-    def calculate_linear_regression(self, data: dict, scope: int, timestamp: int):
+    def calculate_linear_regression(self, data: dict, scope: int, x_value: int):
         # Get list of (timestamp, price) tuples
         data_tuples = list(data.items())
 
         # Get the last scope number of elements from the end of the list
-        last_scope_data = data_tuples[-scope:]
+        last_scope_data = data_tuples[-scope:] # Array slicing, not sure if I've done it right
 
-        # Extract timestamps and prices from the last scope data
-        x_data = np.array([item[0] for item in last_scope_data])
+        # Extract timestamps and prices from the last scope amount of data
+        # x_data is timestamps, y_data is prices
+        x_data = np.array([item[0] for item in last_scope_data]) # Had to get ChatGPT to help me with this one :(
         y_data = np.array([item[1] for item in last_scope_data])
-
         n = np.size(x_data)
 
         x_mean = np.mean(x_data)
         y_mean = np.mean(y_data)
 
+        # Just plugging numbers into formula
         Sxy = np.sum(x_data*y_data)- n*x_mean*y_mean 
         Sxx = np.sum(x_data*x_data)-n*x_mean*x_mean 
 
-        a = Sxy/Sxx 
-        b = y_mean-a*x_mean 
+        m = Sxy/Sxx 
+        c = y_mean-m*x_mean 
 
-        y_pred = a * timestamp + b
+        # Make a prediction, using y = mx + c
+        y_pred = m * x_value + c
         return y_pred
 
     def run(self, state: TradingState):
@@ -67,43 +73,48 @@ class Trader:
             max_buy_volume = self.POSITION_LIMIT[product] - current_position
             max_sell_volume = self.POSITION_LIMIT[product] + current_position
 
-            if len(order_depth.sell_orders) != 0:
-                ask_sum = 0
-                ask_vol = 0
-                for i in range(len(order_depth.sell_orders)):
-                    ask, ask_amount = list(order_depth.sell_orders.items())[i]
-                    ask_sum += int(ask) * int(ask_amount)
-                    ask_vol += int(ask_amount)
-                ask_avg = ask_sum / ask_vol if ask_vol != 0 else 0
-            
-            # Determine average bid price across orders and their corresponding amounts
-            if len(order_depth.buy_orders) != 0:
-                bid_sum = 0
-                bid_vol = 0
-                for i in range(len(order_depth.buy_orders)):
-                    bid, bid_amount = list(order_depth.buy_orders.items())[i]
-                    bid_sum += int(bid) * int(bid_amount)
-                    bid_vol += int(bid_amount)
-                bid_avg = bid_sum / bid_vol if bid_vol != 0 else 0
+            # True Average
+            #if len(order_depth.sell_orders) != 0:
+            #    ask_sum = 0
+            #    ask_vol = 0
+            #    for i in range(len(order_depth.sell_orders)):
+            #        ask, ask_amount = list(order_depth.sell_orders.items())[i]
+            #        ask_sum += int(ask) * int(ask_amount)
+            #        ask_vol += int(ask_amount)
+            #    ask_avg = ask_sum / ask_vol if ask_vol != 0 else 0
+            #
+            ## Determine average bid price across orders and their corresponding amounts
+            #if len(order_depth.buy_orders) != 0:
+            #    bid_sum = 0
+            #    bid_vol = 0
+            #    for i in range(len(order_depth.buy_orders)):
+            #        bid, bid_amount = list(order_depth.buy_orders.items())[i]
+            #        bid_sum += int(bid) * int(bid_amount)
+            #        bid_vol += int(bid_amount)
+            #    bid_avg = bid_sum / bid_vol if bid_vol != 0 else 0
 
             # Get values with greatest trading volume (GTV).
             gtv_ask_price = list(order_depth.sell_orders.items())[-1][0]
             gtv_bid_price = list(order_depth.buy_orders.items())[-1][0]
             
-            if ask_avg and bid_avg:
-                acceptable_price = (ask_avg + bid_avg) / 2
-            else:
-            # Calculate acceptable price using most traded values.
-                acceptable_price = int((gtv_ask_price + gtv_bid_price) / 2)
+            acceptable_price = int((gtv_ask_price + gtv_bid_price) / 2)
 
-            pred_acc_price_20 = self.calculate_linear_regression(self.PRICE_HISTORY[product], 20, state.timestamp)
-            pred_acc_price_100 = self.calculate_linear_regression(self.PRICE_HISTORY[product], 100, state.timestamp)
-            pred_acc_price_global = self.calculate_linear_regression(self.PRICE_HISTORY[product], len(self.PRICE_HISTORY[product]), state.timestamp)
-            print("Predicted Acceptable Price (20): ", pred_acc_price_20)
-            print("Predicted Acceptable Price (100): ", pred_acc_price_100)
+            if len(self.PRICE_HISTORY[product]) > 20:
+                pred_acc_price_20 = self.calculate_linear_regression(self.PRICE_HISTORY[product], 20, state.timestamp)
+                print("Predicted Acceptable Price (20): ", pred_acc_price_20)
+            if len(self.PRICE_HISTORY[product]) > 100:
+                pred_acc_price_100 = self.calculate_linear_regression(self.PRICE_HISTORY[product], 100, state.timestamp)
+                print("Predicted Acceptable Price (100): ", pred_acc_price_100)
+            
+            pred_acc_price_global = self.calculate_linear_regression(self.PRICE_HISTORY[product], (len(self.PRICE_HISTORY[product]) - 2), state.timestamp)
             print("Predicted Acceptable Price (global): ", pred_acc_price_global)
+            
             self.update_price_history(product, acceptable_price, state)
 
+            if len(self.PRICE_HISTORY[product]) > 20:
+                last_twenty_prices = list(self.PRICE_HISTORY[product].values())[-20:]
+                acceptable_price = st.mean(last_twenty_prices)
+            
             print("Acceptable price : " + str(acceptable_price))
             print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
 
